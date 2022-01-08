5768bcd0a8ffb749536a1e77fcb8f7320d6d2eca
Salman Rana <salman.r.rana@gmail.com>
2022-01-08 11:22:04 -0600

    example bundler project and starting jbook project

---
+++ bundler/src/index.js
@@ -0,0 +1,7 @@
+// ## using commonjs formatting
+// const message = require('./message');
+
+// ## using the ES Module formatting
+import message from './message';
+
+console.log(message);
---
+++ bundler/src/message.js
@@ -0,0 +1,5 @@
+// ## using commonjs formatting
+// module.exports = 'Hi There!';
+
+// ## using the ES Module formatting
+export default 'Hi There!';
