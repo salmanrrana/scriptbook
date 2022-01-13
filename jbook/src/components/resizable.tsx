02a097e382474121f30c91e994d4a88258579d61
Salman Rana <salman.r.rana@gmail.com>
2022-01-12 21:10:54 -0600

    bigfix: add copnstraints on min/max height

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -14,6 +14,8 @@ const Resizable: React.FC<ResizableProps> = ({
       height={300}
       width={Infinity}
       resizeHandles={['s']}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 100]}
     >
       {children}
     </ResizableBox>
