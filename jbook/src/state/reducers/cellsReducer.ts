184d8c673c36e11cba8fcf8a68e40a0be64394d2
Salman Rana <salman.r.rana@gmail.com>
2022-01-15 11:49:27 -0600

    refactor: using immer for simple state updates

--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -1,3 +1,5 @@
+
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,24 +20,15 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
+const reducer = produce((
   state: CellsState = initialState,
   action: Action
-): CellsState => {
+) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content
-          },
-        },
-      };
+      state.data[id].content = content;
+      return;
     case ActionType.DELETE_CELL:
       return state;
     case ActionType.INSERT_CELL_BEFORE:
@@ -45,6 +38,6 @@ const reducer = (
     default:
       return state;
   }
-};
+});
 
 export default reducer;
