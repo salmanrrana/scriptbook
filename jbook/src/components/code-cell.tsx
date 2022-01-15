69b8b2bb28d507778918c4eb3a6cd9b92a8b413a
Salman Rana <salman.r.rana@gmail.com>
2022-01-15 14:39:03 -0600

    feature: actionbar component and some dummy data for testing

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -5,14 +5,21 @@ import Resizable from './resizable';
 import Preview from './preview';
 import bundle from '../bundler';
 
-const CodeCell = () => {
-  const [input, setInput] = useState('');
-  const [err, setErr] = useState('');
+import { Cell } from '../state';
+import { useActions } from '../hooks/useActions';
+
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -20,15 +27,15 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1;"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} bundlingErrorStatus={err} />
--- jbook/src/components/text-editor.tsx
+++ jbook/src/components/text-editor.tsx
@@ -1,11 +1,17 @@
 import './text-editor.css';
 import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import { Cell } from '../state';
+import { useActions } from '../hooks/useActions';
 
-const TextEditor: React.FC<any> = () => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -28,14 +34,14 @@ const TextEditor: React.FC<any> = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor value={value} onChange={(rawString) => setValue(rawString || '')} />
+        <MDEditor value={cell.content} onChange={(rawString) => updateCell(cell.id, rawString || '')} />
       </div>
     );
   }
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'Click to edit'} />
       </div>
     </div>
   );
