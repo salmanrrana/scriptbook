d5b62361ac8f72256a1164d54e1a70329e4d540e
Salman Rana <salman.r.rana@gmail.com>
2022-01-15 12:37:41 -0600

    random id generator function + move cell and insert cell reducer

--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -28,20 +28,57 @@ const reducer = produce((
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      return;
+      return state;
     case ActionType.DELETE_CELL:
       // deletes it from the "data" object
       delete state.data[action.payload];
       //deletes it from the "order" array by the "id" using filter
       state.order = state.order.filter(id => id !== action.payload);
-      return;
-    case ActionType.INSERT_CELL_BEFORE:
       return state;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      return state;
+    case ActionType.INSERT_CELL_BEFORE:
+      // creating a cell to add into our state
+      const cell: Cell = {
+        id: randomId(),
+        type: action.payload.type,
+        content: ''
+      };
+
+      state.data[cell.id] = cell;
+
+      // now taking id of the cell and inserting into the "order" array
+      const foundIndex = state.order.findIndex(id => id === action.payload.id);
+      //if null, add to the end of the order
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        // if value, then insert it at spot
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
       return state;
     default:
       return state;
   }
 });
+
+const randomId = () => {
+  // using the "36" allows to use both numbers and letters
+  // substring(2,5) will bring back a portion of the string we created
+  return Math.random().toString(36).substring(2, 6);
+};
 
 export default reducer;
