91d0d4205c683999098ccdfe0b089a0ace1162b8
Salman Rana <salman.r.rana@gmail.com>
2022-01-18 11:28:49 -0600

    feature: run show function only in the cell it is used within

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -21,29 +21,34 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
 
-      const show = (value) => {
-        const root = document.querySelector('#root');
+    var show = (value) => {
+      const root = document.querySelector('#root');
 
-        if (typeof value === 'object') {
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, root)
-          } else {
-            root.innerHTML = JSON.stringify(value);
-          }
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root)
         } else {
-          root.innerHTML = value
+          root.innerHTML = JSON.stringify(value);
         }
+      } else {
+        root.innerHTML = value
       }
-      `
-    ];
+    }
+    `;
+    const showFuncNoop = `var show = () => {}`;
+    let cumulativeCode = [];
 
     for (let cellContent of orderedCells) {
       if (cellContent.type === 'code') {
+        if (cellContent.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(cellContent.content);
       }
       // if iterating on the current id of the cell
