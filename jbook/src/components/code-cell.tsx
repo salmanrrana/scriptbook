940c9a6f1c7f5e9f85328713ef0a0222ff507e6d
Salman Rana <salman.r.rana@gmail.com>
2022-01-18 10:37:34 -0600

    feature: code sharing between cells

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -37,23 +37,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return cumulativeCode;
   });
 
-  console.log({ cumulativeCode });
-
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cell.id, cumulativeCode.join('\n'), createBundle]);
 
   return (
     <Resizable direction='vertical'>
