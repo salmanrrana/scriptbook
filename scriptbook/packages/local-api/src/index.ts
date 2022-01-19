1075760840095133cd89e0a5d74fc9d9ef78436a
Salman Rana <salman.r.rana@gmail.com>
2022-01-18 21:27:01 -0600

    feature: serve react files in development environment

--- scriptbook/packages/local-api/src/index.ts
+++ scriptbook/packages/local-api/src/index.ts
@@ -1,7 +1,29 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
+
+  // The following will give us our react assets when:
+
+  if (useProxy) {
+    // when running the app in development mode
+    app.use(createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent'
+    }));
+  } else {
+    // a user install the cli on their machine
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
